8af9f0bf8c1ff6608433142f0ab86e5ae4f23720
imdhemy <imdhemy@gmail.com>
2023-07-30 12:50:38 +0200

    [style] add color scheme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,19 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = '#4F3A75';
+const tintColorDark = '#FFC720';
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: '#544B64',
+    background: '#FFFFFF',
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: '#D6CAE8',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: '#FFFFFF',
+    background: '#4F3A75',
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: '#FCEDC6',
     tabIconSelected: tintColorDark,
   },
 };
