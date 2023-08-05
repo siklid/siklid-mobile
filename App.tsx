3b449fc4ddb969a24f99df600330a10047432855
imdhemy <imdhemy@gmail.com>
2023-08-05 23:06:41 +0200

    [theme] add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,27 @@
 import Welcome from '@/app/welcome';
+import {SplashScreen} from 'expo-router';
+import {useFonts} from 'expo-font';
+import {useEffect} from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return <Welcome/>;
+  const [loaded] = useFonts({
+    LatinFont: require('./assets/fonts/JosefinSans-Regular.ttf'),
+    ArabicFont: require('./assets/fonts/NotoSansArabic-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+      <Welcome/>
+  );
 }
